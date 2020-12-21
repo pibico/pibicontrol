87c7840756e21b4e07e67385ad1227a8df2b0eb7
pibico <pibico.sl@gmail.com>
2020-12-21 13:29:34 +0100

    modified:   pibicontrol/pibicontrol/api.py 	modified:   pibicontrol/pibicontrol/doctype/sensor/sensor.json

--- pibicontrol/pibicontrol/api.py
+++ pibicontrol/pibicontrol/api.py
@@ -6,6 +6,11 @@ from __future__ import unicode_literals
 import frappe
 from pibicontrol.pibicontrol.doctype.mqtt_settings.mqtt_settings import send_mqtt
 from frappe.utils import cstr
+import datetime
+
+DATE_FORMAT = "%Y-%m-%d"
+TIME_FORMAT = "%H:%M:%S.%f"
+DATETIME_FORMAT = DATE_FORMAT + " " + TIME_FORMAT
 
 @frappe.whitelist()
 def get_image(slide):
@@ -15,15 +20,20 @@ def get_image(slide):
   return data
 
 def ping_devices_via_mqtt():
-  command = "is_alive"
   sensors = frappe.get_list(
     doctype = "Sensor",
-    fields = ['hostname'],
+    fields = ['hostname','lastseen'],
     filters = [['docstatus', '<', 2], ['disabled', '=', 0]]
   )
   if sensors:
     hostnames = []
+    command = "is_alive"
     for device in sensors:
-      hostnames.append(device.hostname + "/mqtt")
-    send_mqtt(hostnames, cstr(command))
-  
+      lastseen = device.lastseen
+      now = datetime.datetime.now()
+      time_minutes = (now - lastseen).total_seconds()/60
+      if 10 >= time_minutes > 5:
+        hostnames.append(device.hostname + "/mqtt")
+        send_mqtt(hostnames, cstr(command))
+      elif time_minutes > 10:
+        frappe.msgprint("[INFO] Not receiving data from " + device.hostname)
